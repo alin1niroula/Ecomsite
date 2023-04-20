c7f6a92234c215d35199014eb80a1b4508bb113a
alin1niroula <alin1niroula@gmail.com>
2023-04-20 12:35:34 +0100

    all done

--- OnlineShop/views.py
+++ OnlineShop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
+from slider.models import Slider
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
+    sliderdata = Slider.objects.all()
 
     # Get the reviews
     reviews = None
@@ -12,5 +14,6 @@ def home(request):
     context = {
         'products': products,
         'reviews': reviews,
+        'slider':sliderdata,
     }
     return render(request, 'home.html', context)
